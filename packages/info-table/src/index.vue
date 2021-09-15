317bd456a7dabad137a66f018e1b90f5786f8cef
zhangxiang <502123065@qq.com>
2021-09-15 08:53:53 +0800

    feat: init info-table

---
+++ packages/info-table/src/index.vue
@@ -0,0 +1,20 @@
+<!--
+  @author: zhangxiang
+  @date: 2021-09-15 08:53:32
+  @desc：
+-->
+<template>
+  <div>info-table</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped></style>
